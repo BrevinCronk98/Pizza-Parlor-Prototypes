5e4d93755f9632591c096d6a43be1f7f09ae98f5
brevin <cronkbrevin98@gmail.com>
2020-02-14 03:04:25 -0800

    final

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Marios Pizza Parlor
 ### By Brevin Cronk
 
-## Descriptiom
+## Description
 ### Marios Pizza Parlor is a webpage I created for our prototypes and constructors portion of our intermediate JavaScript course. You are able to pick out toppings and sizes for your pizza and the price caries depending on the size.
+
+## Specifications:
+### Takes the input of the topping checkboxes, pushes them into an array, turns the array into text, and displays the text.
+* ### Input: Pepperoni: Checked, Olives: Checked, Chicken : Unchecked.
+* ### Output "Toppings: Pepperoni, Chicken
+### Price varies depending on pizza size with a simple formula.
+* ### Input: 12 Inch,
+* ### Output: Price is 12 Dollars. 18 inch would be 15 dollars.
 
 ## Known Bugs
 ### So far I am unable to make it so you can pick multiple toppings. It only works with 1 topping. Will correct soon.
@@ -10,10 +18,5 @@
 ## Setup Instructions
 ### You can access my live webpage via GH-Pages at
 ### If you would like to you can also download my code as a zip file and modify it yourself.
-
-## Specifications
-* # Allow user to choose pizza size and toppings.
-* # Create a pizza object constructor with properties for toppings and size.
-* # Create a prototype method for the cost of a pizza depending on selections chosen.
 
 ## Copyright (c) 2020 Brevin Cronk
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 function Pizza(size){
-    this.size = size,
-    this.price = 15,
-    this.toppings = ""
+    this.size = size;
+    this.price = 15;
+    this.toppings = [];
+    
 }
 
 Pizza.prototype.sizePrice = function() {
@@ -9,41 +10,57 @@ Pizza.prototype.sizePrice = function() {
         this.price -= 3;
     } else if(this.size=="size2"){
         this.price==this.price;
+
     }   
 }
 
-Pizza.prototype.addTopping = function(inputTop){
-    this.toppings = inputTop;
-   
-}
-
-Pizza.prototype.showPizza = function(showOutputDiv){
-    var showOutputMsg = showOutputDiv;
-    var pizzaDetails = "";
-    pizzaDetails += `<p> Thank you for purchasing on of our pizzas</p>`
-    pizzaDetails += `<p> $${this.price}</p>`;
-    pizzaDetails += `<p>${this.toppings}</p>`;
-    showOutputMsg.html(pizzaDetails);
-}
-
+    Pizza.prototype.showPizza = function(showOutputDiv){
+        var showOutputMsg = showOutputDiv;
+        var pizzaDetails = "";
+        pizzaDetails += `<p> Thank you for purchasing one of our pizzas</p>`
+        pizzaDetails += `<p> $${this.price}</p>`;
+        showOutputMsg.html(pizzaDetails);
+    }
+    
 $(document).ready(function(){
+    
+    function getValue(){
+        this.price = 15
+        var checks = document.getElementsByClassName('checks')
+        this.toppings = [];
+        for (i = 0; i<3; i++){
+            if(checks[i].checked === true){
+                this.toppings.push(checks[i].value);
+                
+            } else if(this.toppings.length === 3){
+                this.price += 3;
+            } 
+        }
+    }
+    
+    function displayTopping(){
+        $("#grandTotal").text(`Toppings: ${this.toppings}`)
+    }
+    
     $("button#size1").click(function(event){
         event.preventDefault();
         var pizza = new Pizza("size1");
         pizza.sizePrice();
-        var inputTop = $("input:radio[name=topping]:checked").val();  
-        pizza.addTopping(inputTop)
-        pizza.showPizza($("#grandTotal"));
+        pizza.showPizza($("#pizza"));
+        getValue();
+        displayTopping();    
     });
     $("button#size2").click(function(event){
         event.preventDefault();
         var pizza = new Pizza("size2");
         pizza.sizePrice();
-        var inputTop = $("input:radio[name=topping]:checked").val(); 
-        pizza.addTopping(inputTop)
-        pizza.showPizza($("#grandTotal"));
+        pizza.showPizza($("#pizza"));
+        getValue();
+        displayTopping();
     });
-})
+});
+    
+
 
 
 
